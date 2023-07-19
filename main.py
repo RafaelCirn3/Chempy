4a309fbf5ffc26085814fe6fe78a3a98420497c2
Rafael Cirne <125037856+RafaelCirn3@users.noreply.github.com>
2023-07-19 13:57:39 -0300

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-from questoes import informacoeselem
-
-print('Olá, seja bem vindo ao próximo nível de aprendizado para melhores estudos')
-start = input('podemos começar ?[S/N] ')
-if start.upper() == 'S':
-    print('começaremos com uma pequena revisão sobre elementos')
-    informacoeselem
-else:
-    print('até logo!')
